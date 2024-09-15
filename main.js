399d7a5710b41d3fa392f4230a3f41f2132c6a68
Untoast <100155333+Untoast@users.noreply.github.com>
2024-09-15 08:40:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const profileimg = document.querySelector('.profile img');
+profileimg.src = 'https://avatars.githubusercontent.com/u/100155333?v=4&size=120';
+
+const name = document.querySelector('#name');
+name.textContent = 'Untoast';
+
+const wordle = document.querySelector('#wordle');
+wordle.src = 'Wordle.png';
