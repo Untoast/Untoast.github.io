4e8f83a4119a8f78cd83c8dc7ef3ab6236b3b783
Untoast <100155333+Untoast@users.noreply.github.com>
2024-09-17 01:33:50 -0400

    Update Main.js

--- WordleButBad/Main.js
+++ WordleButBad/Main.js
@@ -425,3 +425,4 @@ function checkViewport() {
 // Check viewport on load and on resize
 window.addEventListener('load', checkViewport);
 window.addEventListener('resize', checkViewport);
+window.addEventListener('orientationchange', checkViewport);
