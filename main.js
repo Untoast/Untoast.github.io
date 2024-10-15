467b770f2a3929d140c8983ef3ba1758891d35fa
Untoast <100155333+Untoast@users.noreply.github.com>
2024-10-15 03:19:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const button = document.getElementById('wordle');
 
 button.addEventListener('click', () => {
-    window.location.href = 'https://untoast.github.io/WordleButBad/';
+    window.location.href = 'https://untoast.github.io/Wordle';
 });
